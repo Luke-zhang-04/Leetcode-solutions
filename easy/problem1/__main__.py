bf4bfc3e2743fadb5c3646dcdc4be2c02cdb0fc1
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-08-16 21:56:32 -0400

    adhere to conventions, use Typescript
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ easy/problem1/__main__.py
@@ -0,0 +1,19 @@
+"""
+Runtime: 1212 ms, faster than 23.82% of Python3 online submissions for Two Sum.
+Memory Usage: 14.6 MB, less than 99.45% of Python3 online submissions for Two Sum.
+08/16/2019
+DD/MM/YY
+"""
+
+
+class Solution:
+    def twoSum(self, nums: list, target: int) -> list:
+        for i in nums:
+            if target - i in nums:
+                if nums.count(target - i) >= 2:
+                    indices = [index for index, x in enumerate(nums) if x == target - i]
+                    return [indices[0], indices[1]]
+                elif nums.index(i) == nums.index(target - i):
+                    continue
+                else:
+                    return [nums.index(i), nums.index(target - i)]
