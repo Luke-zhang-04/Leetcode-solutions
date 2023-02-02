2938777ef103046f82fbc156b6c5eae0db26b0f1
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2023-02-02 10:11:26 -0500

    update problem 2

--- medium/problem2/main.go
+++ medium/problem2/main.go
@@ -1,16 +1,12 @@
 package main
 
-/*
-Runtime: 12 ms, faster than 65.70% of Go online submissions for Add Two Numbers.
-Memory Usage: 5.9 MB, less than 7.32% of Go online submissions for Add Two Numbers.
-17/12/2019
-DD/MM/YY
-*/
+// This implementation uses the existing lists instead of creating a new one
 
-import (
-	"math/big"
-	"strconv"
-)
+/*
+Runtime: 11 ms, beats 72% of Go submissions
+Memory Usage: 4.3 MB, beats 100% of Go submissions
+2023-02-02
+*/
 
 // ListNode Definition for singly-linked list.
 type ListNode struct {
@@ -18,54 +14,53 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-
-	return string(runes)
-}
-
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var carry int
+	l1Len := 0
+	l2Len := 0
+	curNode1 := l1
+	curNode2 := l2
+	var lastNode1, lastNode2 *ListNode
 
-	if l1 == nil || l1.Val == 0 && l1.Next == nil {
-		return l2
+	for curNode1 != nil || curNode2 != nil {
+		total := carry
+
+		if curNode1 != nil {
+			total += curNode1.Val
+		}
+		if curNode2 != nil {
+			total += curNode2.Val
+		}
+		carry = total / 10
+		total -= carry * 10
+
+		if curNode1 != nil {
+			curNode1.Val = total
+			lastNode1 = curNode1
+			curNode1 = curNode1.Next
+			l1Len++
+		}
+		if curNode2 != nil {
+			curNode2.Val = total
+			lastNode2 = curNode2
+			curNode2 = curNode2.Next
+			l2Len++
+		}
 	}
-	if l2 == nil || l2.Val == 0 && l2.Next == nil {
+
+	if l1Len > l2Len {
+		if carry != 0 {
+			newNode := &ListNode{carry, nil}
+			lastNode1.Next = newNode
+		}
+
 		return l1
 	}
 
-	var values1, values2 string
-	for l1 != nil {
-		values1 += strconv.Itoa(l1.Val)
-		l1 = l1.Next
-	}
-	for l2 != nil {
-		values2 += strconv.Itoa(l2.Val)
-		l2 = l2.Next
+	if carry != 0 {
+		newNode := &ListNode{carry, nil}
+		lastNode2.Next = newNode
 	}
 
-	intValues1 := new(big.Int)
-	intValues2 := new(big.Int)
-
-	intValues1, _ = intValues1.SetString(reverse(values1), 10)
-	intValues2, _ = intValues2.SetString(reverse(values2), 10)
-
-	sum := new(big.Int)
-	sum.Add(intValues1, intValues2)
-	final := reverse(sum.String())
-	result := &ListNode{0, nil}
-
-	foo, _ := strconv.Atoi(string(final[0]))
-	res := &ListNode{Val: foo}
-	result.Next = res
-
-	for _, i := range final[1:] {
-		foo, _ := strconv.Atoi(string(i))
-		res.Next = &ListNode{Val: foo}
-		res = res.Next
-	}
-
-	return result.Next
+	return l2
 }
